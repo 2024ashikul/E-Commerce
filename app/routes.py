a7eaa4908c0cc311235a939b52ba52afce72abae
2024ashikul <2024ashikul@gmail.com>
2025-03-20 02:53:56 +0600

    adding api for registering

--- app/routes.py
+++ app/routes.py
@@ -84,6 +84,47 @@ def register():
     except:
         return "FAILED TO GET USER"
 
+@main.route('/api/register', methods=['POST'])
+def registeri():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+    name = request.form.get('name')
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    print("come here")
+    if not all([name, username, email, password]):
+        return jsonify({'error': 'Missing form data'}), 400
+
+    hashed_pass = generate_password_hash(password, method='pbkdf2:sha512', salt_length=16)
+    file = request.files['file']
+    file_extension = file.filename.rsplit('.',1)[1].lower()
+    unique_extension = generate_unique_filename(file_extension)
+
+    filename = os.path.join(UPLOAD_FOLDER, unique_extension)
+    file.save(filename)
+
+    user = User(name = name , username= username, email = email, password = hashed_pass, profile_pic= filename)
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except:
+        return jsonify({"error"}),400
+
+    user_id = 123
+
+    return jsonify({
+        'message': 'User successfully registered',
+        'user_id': user_id,
+        'profile_pic': filename
+    }), 201
+
+
+
+
+
+
+
 def generate_unique_filename(extension):
     while True:
         unique_filename = str(uuid.uuid4()) + '.' + extension
