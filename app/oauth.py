9b2002cb3e58a20da426a97b68a718864091db29
2024ashikul <2024ashikul@gmail.com>
2025-03-19 10:47:42 +0600

    adding google login functionality

--- app/oauth.py
+++ app/oauth.py
@@ -7,8 +7,7 @@ def configure_oauth(app):
 
     oauth.register(
         name="google",
-        client_id=os.getenv('client_id'),
-        client_secret=os.getenv('client_secret'),
+        
         authorize_url="https://accounts.google.com/o/oauth2/auth",
         access_token_url="https://oauth2.googleapis.com/token",
         userinfo_endpoint="https://openidconnect.googleapis.com/v1/userinfo",
